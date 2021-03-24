79d0952243ed21dfb20bfa7f5a3e6a85f2079675
StriMer22 <homen932@gmal.com>
2021-03-24 16:29:33 +0100

    kek

--- js/main-collection.js
+++ js/main-collection.js
@@ -40,6 +40,25 @@ const productsMen = `
 ]
 `;
 
+const productsShoes = `
+[
+    {
+        "title": "Puma Sports Bra",
+        "price": 12.00,
+        "image": "main-collection-shoes-one.jfif"
+    },
+    {
+        "title": "Reebok Long Sleave Top",
+        "price": 18.00,
+        "image": "main-collection-shoes-one.jfif"
+    },
+    {
+        "title": "Quicksilver Workout Top",
+        "price": 12.00,
+        "image": "main-collection-shoes-one.jfif"
+    }
+]
+`;
 
 function prodWomen(productsWomen) {
     const products = document.querySelector('.main-collection-content');
@@ -71,15 +90,32 @@ function prodMen(productsMen) {
     }
 }
 
+function prodShoes(productsShoes) {
+    const products = document.querySelector('.main-collection-content');
+    products.innerHTML = '';
+    for (const product of productsShoes) {
+        products.innerHTML += `
+        <article class="main-collection-item">
+                    <img src="img/${product.image}" alt="${product.title}" class="main-collection-image">
+                    <a class="main-collection-name">${product.title}</a>
+                    <p class="main-collection-price">$${product.price},00USD</p>
+                    <button class="main-collection-button">Add to Cart</button>
+                </article>
+        `
+    }
+}
+
 
 
 prodWomen(JSON.parse(productsWomen));
 prodMen(JSON.parse(productsMen));
+prodShoes(JSON.parse(productsMen));
 
 const womenBtn = document.getElementById('tab-women-button');
 const menBtn = document.getElementById('tab-men-button');
+const shoesBtn = document.getElementById('tab-shoes-button');
 
 womenBtn.addEventListener('click', () => prodWomen(JSON.parse(productsWomen)));
 menBtn.addEventListener('click', () => prodMen(JSON.parse(productsMen)));
-
+shoesBtn.addEventListener('click', () => prodShoes(JSON.parse(productsShoes)));
 
