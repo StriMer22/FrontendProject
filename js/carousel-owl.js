ad714722f6088c20563496bd9eea3c413aa83683
StriMer22 <homen932@gmal.com>
2021-03-27 15:28:03 +0100

    Finished html and css

--- js/carousel-owl.js
+++ js/carousel-owl.js
@@ -1,4 +1,21 @@
-$('.owl-one').owlCarousel({
+$('.owl').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+$('.second-owl').owlCarousel({
     loop: true,
     margin: 10,
     responsiveClass: true,
