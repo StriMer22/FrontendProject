9c82addff8ca7e225d7b3ba0436c4a1d5938a63a
StriMer22 <homen932@gmal.com>
2021-03-29 16:56:38 +0200

    test

--- js/product-list-MS.js
+++ js/product-list-MS.js
@@ -1,19 +1,20 @@
 class ProductList {
     constructor(cart) {
-      this.cart = cart;
-      this.container = document.querySelector(".mid-season-collection");
-      this.productService = new ProductsService();
-      this.productService
-        .getProducts()
-        .then(() => this.renderProducts())
-        .then(() => this.addEventListeners());
+        this.cart = cart;
+        this.container = document.querySelector(".mid-season-collection");
+        this.productService = new ProductsService();
+        this.productService
+            .getProducts()
+            .then(() => this.renderProducts())
+            .then(() => this.addEventListeners());
     }
+
     async renderProducts() {
-      let productListDomString = "";
-      const products = await this.productService.getProducts();
-      [...products]
-        .forEach((product) => {
-          productListDomString += `<div class="season-item">
+        let productListDomString = "";
+        const products = await this.productService.getProducts();
+        [...products]
+            .forEach((product) => {
+                productListDomString += `<div class="season-item">
                         <a href="item-page.html">
                         <img src="img/${product.image}" alt="${product.title}" class="item-img">
                         <span class="menu-nav">${product.title}</span>
@@ -21,20 +22,23 @@ class ProductList {
                         <span class="menu-price">$${product.price},00USD</span>
                         <a href="#" class="btn btn-primary basket-shop-btn" data-id="${product.id}">Add to Cart</a>
                  </div>`;
-        });
-      this.container.innerHTML = productListDomString;
+            });
+        this.container.innerHTML = productListDomString;
     }
+
     async addEventListeners() {
         document
-        .querySelectorAll('.basket-shop-btn')
-        .forEach(button =>
-            button.addEventListener('click', event =>
-            this.handleProductBuyClick(event)
-            )
-        );
-    }     handleProductBuyClick(event) {
-            const button = event.target;
-            const id = button.dataset.id;
-            this.cart.addProduct(id);
-        }
+            .querySelectorAll('.basket-shop-btn')
+            .forEach(button =>
+                button.addEventListener('click', event =>
+                    this.handleProductBuyClick(event)
+                )
+            );
+    }
+
+    handleProductBuyClick(event) {
+        const button = event.target;
+        const id = button.dataset.id;
+        this.cart.addProduct(id);
+    }
 }
